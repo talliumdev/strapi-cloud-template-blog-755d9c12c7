0f012cde6f1e95256a3a2cd7997fe66c955f3595
maximbondarenko <maximbndrnk@gmail.com>
2026-06-24 13:03:14 +0300

    add 2 side panel menus

---
+++ src/admin/app.js
@@ -0,0 +1,40 @@
+const config = {
+  locales: [],
+};
+
+const bootstrap = (app) => {
+  app.addMenuLink({
+    to: '/content-manager/collection-types/api::project-request.project-request',
+    icon: 'envelop',
+    intlLabel: {
+      id: 'project-request.menu.label',
+      defaultMessage: 'Project Requests',
+    },
+    permissions: [
+      {
+        action: 'plugin::content-manager.explorer.read',
+        subject: 'api::project-request.project-request',
+      },
+    ],
+  });
+
+  app.addMenuLink({
+    to: '/content-manager/collection-types/api::candidate-application.candidate-application',
+    icon: 'user',
+    intlLabel: {
+      id: 'candidate-application.menu.label',
+      defaultMessage: 'Candidate Applications',
+    },
+    permissions: [
+      {
+        action: 'plugin::content-manager.explorer.read',
+        subject: 'api::candidate-application.candidate-application',
+      },
+    ],
+  });
+};
+
+export default {
+  config,
+  bootstrap,
+};
